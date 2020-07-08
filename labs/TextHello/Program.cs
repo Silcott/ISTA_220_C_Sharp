a56d5a598c125093a25b8f8f92679d243910ca32
James.Silcott <silcott.jb@outlook.com>
2020-07-08 11:33:17 -0400

    my first commit

---
+++ labs/TextHello/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace TextHello //namespace is an object
+{ //curly bracket or braces
+    public class Program //public, protected, or private -
+                         //a class is a blueprint or design -
+                         //Program is the name of the class
+    { //saves the stack that is higher and allocates a new stack
+        public static void Main(string[] args) //added public but doesn't need it -
+                                        //static line - pulls the shoot for you and you can only have one
+                                        //void - returns nothing
+                                        //Main is the name and it can be anything, unless
+                                        //you dont have a Main method already
+                                        //parameter list, args is the name of the parameter (just a name)
+                                        //
+        {
+            string appelation; //declares a variable of a type string which is named name
+            System.Console.Write("Enter your name: "); //System is the namespace
+                                                      //Console is the class
+            appelation = System.Console.ReadLine(); //Readline method reads the line and belongs to Console Class
+            System.Console.WriteLine($"Hello, {appelation}"); //Calls writeline method
+                                                              //$ means string interpulation
+        }//this de-allocates and whatever is on the top level is restored
+
+
+    }//this de-allocates and whatever is on the top level is restored
+}//this is de-allocated but nothing is higher sot he program terminates
---
+++ quizzes/Quiz1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Quiz1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is C Sharp quiz 1.");
+            Console.WriteLine("PART 1");
+            Console.WriteLine("MM         MM  SSSSSSSS  SSSSSSSS     AAAAAA\n" +
+                              "MMM       MMM  SSSSSSSS  SSSSSSSS     AA  AA\n" +
+                              "MMMM     MMMM  SS        SS          AA    AA\n" +
+                              "MM MM  MM  MM  SS        SS          AA    AA\n" +
+                              "MM  MMMM   MM  SSSSSSSS  SSSSSSSS    AAAAAAAA\n" +
+                              "MM   MM    MM  SSSSSSSS  SSSSSSSS    AAAAAAAAA\n" +
+                              "MM         MM        SS        SS   AA       AA\n" +
+                              "MM         MM        SS        SS   AA       AA\n" +
+                              "MM         MM  SSSSSSSS  SSSSSSSS   AA       AA\n" +
+                              "MM         MM  SSSSSSSS  SSSSSSSS   AA       AA\n" +
+                              "");
+
+            Console.WriteLine("PART 2");
+            double phi = (1 + Math.Sqrt(5)) / 2;
+            Console.WriteLine($"phi is {phi}");
+        }
+    }
+}
